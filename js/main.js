646b6deb62828806910856dcf85435c3eda02ea6
Zach <zpk1997@live.unc.edu>
2017-10-08 17:25:35 -0400

    added js, images, etc.

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.getElementById('any-id').innerHTML = "Current Status of Overall Satisfaction of my Classes: Pretty Good";
+
+ document.getElementById('last-update').innerHTML =  "Page last updated on " + document.lastModified;
